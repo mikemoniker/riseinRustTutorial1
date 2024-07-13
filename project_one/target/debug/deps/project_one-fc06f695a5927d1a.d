b1a99a4e33ed13e1cc36fadcddf70fd4c05b1a4d
Jordan <jmcarlile@ucdavis.edu>
2024-07-13 01:00:16 +0000

    complete

---
+++ project_one/target/debug/deps/project_one-fc06f695a5927d1a.d
@@ -0,0 +1,5 @@
+/workspace/riseinRustTutorial1/project_one/target/debug/deps/project_one-fc06f695a5927d1a: src/main.rs
+
+/workspace/riseinRustTutorial1/project_one/target/debug/deps/project_one-fc06f695a5927d1a.d: src/main.rs
+
+src/main.rs:
---
+++ project_one/target/debug/project_one.d
@@ -0,0 +1,1 @@
+/workspace/riseinRustTutorial1/project_one/target/debug/project_one: /workspace/riseinRustTutorial1/project_one/src/main.rs
